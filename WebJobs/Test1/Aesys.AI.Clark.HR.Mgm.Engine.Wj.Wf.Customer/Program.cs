afcea6d48a3267a2fca11d9dcaa501ca516ec120
Armando Pitotti <armando.pitotti@aesys.tech>
2019-07-26 12:09:33 +0200

    first commit

---
+++ WebJobs/Test1/Aesys.AI.Clark.HR.Mgm.Engine.Wj.Wf.Customer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aesys.AI.Clark.HR.Mgm.Engine.Wj.Wf.Customer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
